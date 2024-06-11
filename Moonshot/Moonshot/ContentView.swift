0c1beef22159f2ea209fdf17036ae9c6483857dd
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-11 15:53:20 +0900

    Feat: create NavigationLink

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,26 +7,16 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-    var body: some View {
-        Text(text)
-    }
-    init(text: String) {
-        self.text = text
-        print("create \(text)")
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack {
-                ForEach(1..<100) {
-                    CustomText(text: "item \($0)")
-                }
+        NavigationStack {
+            NavigationLink {
+                Text("Detail View")
+            } label: {
+                Label("tap to see Detail View", systemImage: "arrow.right.circle")
+                    .font(.title)
             }
-            .frame(maxHeight: .infinity)
+            .navigationTitle("Main View")
         }
     }
 }
