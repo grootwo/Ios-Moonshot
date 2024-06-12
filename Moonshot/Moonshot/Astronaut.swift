4cc97e73477af6b2a020abe60ce23385d912bb65
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-12 12:39:16 +0900

    Feat: creat Astronaut

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Groo on 6/12/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
