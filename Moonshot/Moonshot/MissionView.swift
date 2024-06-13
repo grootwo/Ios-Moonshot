fc7abbf03f0508fdf5ba5f1f0623f71ff46a7206
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-13 15:43:21 +0900

    Feat: merge role and crew member
    With error:
    Showing All Errors Only
    Undefined symbol: Moonshot.MissionView.init(mission: Moonshot.Mission) -> Moonshot.MissionView

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
     let mission: Mission
+    let crew: [CrewMember]
     var body: some View {
         ScrollView {
             VStack {
@@ -30,10 +35,22 @@ struct MissionView: View {
         .navigationBarTitleDisplayMode(.inline)
         .background(.darkBackground)
     }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
 }
 
 #Preview {
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    return MissionView(mission: missions[0])
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    return MissionView(mission: missions[0], astronauts: astronauts)
         .preferredColorScheme(.dark)
 }
