cec55c04c2a70ecfe5d4292573532c6765a723e3
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-14 15:05:01 +0900

    Feat: set List version and make toggle

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isGridView = false
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
@@ -16,7 +17,7 @@ struct ContentView: View {
         ]
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: columns) {
+                if isGridView {                    LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
                             MissionView(mission: mission, astronauts: astronauts)
@@ -46,10 +47,49 @@ struct ContentView: View {
                         .padding([.horizontal, .bottom])
                     }
                 }
+                } else {
+                    LazyVStack {
+                        ForEach(missions) { mission in
+                            NavigationLink {
+                                MissionView(mission: mission, astronauts: astronauts)
+                            } label: {
+                                HStack {
+                                    Image(mission.image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 50, height: 50)
+                                        .padding()
+                                    VStack {
+                                        Text(mission.displayName)
+                                            .font(.headline)
+                                        Text(mission.formattedLaunchDate)
+                                            .font(.caption)
+                                    }
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(.lightBackground)
+                                }
+                                .clipShape(.rect(cornerRadius: 10))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.lightBackground)
+                                )
+                            }
+                            .padding([.horizontal, .bottom])
+                        }
+                    }
+                }
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar(content: {
+                Button(action: {
+                    isGridView.toggle()
+                }, label: {
+                    Text(isGridView ? "View as a List" : "View as a Grid")
+                })
+            })
         }
     }
 }
